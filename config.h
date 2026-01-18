c44bfa4aa7b5099fa04a2d5da44a7c29f9ce88a0
Miu <155429921+miublue@users.noreply.github.com>
2026-01-18 13:03:58 -0300

    update config.h

--- config.h
+++ config.h
@@ -2,13 +2,16 @@
 #define _CONFIG_H
 
 #define MOD GDK_CONTROL_MASK|GDK_SHIFT_MASK
-#define FONT "ShureTechMono Nerd Font 16"
-#define ALPHA 0.95
-#define SHELL "/bin/bash"
+#define TITLE "Terminal"
+#define FONT "Terminess Nerd Font 25"
+#define ALPHA 1.0
+#define SHELL "/bin/mksh"
+#define WIDTH 900
+#define HEIGHT 540
 
 const char *colors[16] = {
-    "#101010", "#ff5599", "#99ff55", "#ff9955", "#5599ff", "#ff5599", "#666666", "#eeeeee",
-    "#101010", "#ff5599", "#99ff55", "#ff9955", "#5599ff", "#ff5599", "#666666", "#eeeeee",
+    "#000000", "#ff5faf", "#5fffaf", "#ffaf5f", "#87d7ff", "#ff5faf", "#87d7ff", "#eeeeee",
+    "#808080", "#ff5faf", "#5fffaf", "#ffaf5f", "#87d7ff", "#ff5faf", "#87d7ff", "#eeeeee",
 };
 
 #endif
